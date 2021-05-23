12f3e75c5171eddbfcdf4a5232a2200fdcad3988
PedroLuis <pedroluis0542@gmail.com>
2021-05-23 00:51:11 -0400

    final commit for test project

--- components/Container.js
+++ components/Container.js
@@ -2,17 +2,15 @@ import Head from 'next/head'
 import React from "react";
 import Navigation from "./Navigation";
 
-
-
 const Container = (props) => {
   return (
     <div>
         <Head>
             <title>Next.js Project</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous"></link>
         </Head>
       <Navigation />
-      <div className="container pd-4">
+      <div className="container p-4">
           {props.children}
       </div>
     </div>
--- components/users.js
+++ components/users.js
@@ -1,14 +1,32 @@
+import  Router  from "next/router";
 import React from "react";
+
 
 const Users = (props) => {
   // console.log(props.users)
   return (
     <div>
-      <ul>
+      <ul className="list-group">
         {props.users.map((user, index) => (
-          <li className="list-group-item list-group-item-action" key={index}>
-            <h5>{user.first_name} {user.last_name}</h5>
-            <p>{user.email}</p>
+          <li className="list-group-item   list-group-item-action" key={index} onClick={()=>{
+            Router.push('/users/[id]',`/users/${user.id}`)
+          }}>
+            <div class="d-flex justify-content-between align-center">
+              <div className="flex-grow-1 ms-3">
+                <h5>
+                  {user.id}. {user.first_name} {user.last_name}
+                </h5>
+                <p>{user.email}</p>
+              </div>
+
+              <div class="flex-shrink-0">
+                <img
+                  src={user.avatar}
+                  alt={user.first_name + user.last_name}
+                  style={{ borderRadius: "50%" }}
+                />
+              </div>
+            </div>
           </li>
         ))}
       </ul>
